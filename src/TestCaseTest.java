9ba794d2c94c08eab5283892b4f8d09448b82c14
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:20:24 +0500

    Chapter 22: Added Log for  failed results
    Chapter 22: Added Log for  failed results

--- src/TestCaseTest.java
+++ src/TestCaseTest.java
@@ -12,23 +12,31 @@ public class TestCaseTest extends TestCase {
  		test.run();
  		System.out.println(test.log.equals("setUp testMethod tearDown "));
  	}
+ 	
+ 	public void testResult(){
+ 		WasRun test = new WasRun("testMethod");
+ 		TestResult result = test.run();
+ 		System.out.println("1 run, 0 failed".equals(result.summary()));
+ 	}
+ 	
+ 	public void testFailedResult() {
+ 		WasRun test = new WasRun("testBrokenMethod");
+ 		TestResult result = test.run();
+ 		System.out.println("1 run, 1 failed".equals(result.summary()));
+ 	}
 	
-	public void testResult(){
-		WasRun test = new WasRun("testMethod");
-		TestResult result = test.run();
-		System.out.println("1 run, 0 failed".equals(result.summary()));
-	}
-	
-	public void testFailedResult() {
-		WasRun test = new WasRun("testBrokenMethod");
-		TestResult result = test.run();
-		System.out.println("1 run, 1 failed".equals(result.summary()));
+	public void testFailedResultFormatting() {
+		TestResult result = new TestResult();
+		result.testStarted();
+		result.testFailed();
+    	System.out.println("1 run, 1 failed".equals(result.summary()));
 	}
  
  	public static void main(String[] args) {
  		new TestCaseTest("testTemplateMethod").run();
-		new TestCaseTest("testResult").run();
-		new TestCaseTest("testFailedResult").run();
+ 		new TestCaseTest("testResult").run();
+ 		new TestCaseTest("testFailedResult").run();
+		new TestCaseTest("testFailedResultFormatting").run();
  	}
  
  }
