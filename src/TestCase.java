a659ca3076fcc0e45fa3bbd2ee10e44bd417b043
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:29:43 +0500

    Chapter 23: Can run multiple test.
    Chapter 23: Can run multiple test.

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,36 +1,34 @@
- import java.lang.reflect.Method;
- 
- public class TestCase {
- 
- 	String methodName;
- 
- 	public TestCase(String methodName) {
- 		this.methodName = methodName;
- 	}
- 
- 	public void setUp() {
- 
- 	}
- 	
- 	public void tearDown() {
- 
- 	}
- 
- 	public TestResult run() {
- 		TestResult result = new TestResult();
- 		Class cls = this.getClass();
- 		try {
- 			this.setUp();
- 			result.testStarted();
- 			Method m = cls.getDeclaredMethod(methodName, null);
- 			m.invoke(this);
--			this.tearDown();
- 		} catch (Exception e) {
--			e.printStackTrace();
-+			result.testFailed();
- 		}
-+		this.tearDown();
- 		return result;
- 	}
- 
- }
+package tddPart2;
+
+import java.lang.reflect.Method;
+
+public class TestCase {
+
+	String methodName;
+
+	public TestCase(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public void setUp() {
+
+	}
+	
+	public void tearDown() {
+
+	}
+
+	public void run( TestResult result ) {
+		Class cls = this.getClass();
+		try {
+			this.setUp();
+			result.testStarted();
+			Method m = cls.getDeclaredMethod(methodName, null);
+			m.invoke(this);
+		} catch (Exception e) {
+			result.testFailed();
+		}
+		this.tearDown();
+	}
+
+}
