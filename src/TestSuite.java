8f7fc894bb7be5f23c7eb362a5fcb1722a937313
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:23:19 +0500

    Chapter 22: Can run multiple test.
    Chapter 22: Can run multiple test.

---
+++ src/TestSuite.java
@@ -0,0 +1,26 @@
+package tddPart2;
+
+
+import java.util.ArrayList;
+
+
+public class TestSuite {
+	
+	ArrayList<TestCase> tests;
+	
+	public TestSuite() {
+		tests = new ArrayList<TestCase>();
+	}
+	
+	public void add( TestCase test ) {
+		this.tests.add(test);
+	}
+
+	public void run( TestResult result ) {
+		for( TestCase test : tests) {
+			test.run( result );
+		}
+	}
+	
+	
+}
