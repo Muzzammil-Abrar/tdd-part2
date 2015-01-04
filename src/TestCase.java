71b4be2d5b1712e35c70a926dcc37a2e92c5af77
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:07:07 +0500

    Chapter 21: Report Collected Results
    Chapter 21: Report Collected Results

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,36 +1,36 @@
 package tddPart2;
 
 import java.lang.reflect.Method;
-
-public class TestCase {
-
-	String methodName;
-
-	public TestCase(String methodName) {
-		this.methodName = methodName;
-	}
-	
-
-	public void setUp() {
-		
-
-	}
-	
-	public void tearDown() {
-
-	}
-
-	public void run() {
-		Class cls = this.getClass();
-		try {
-			this.setUp();
-			Method m = cls.getDeclaredMethod(methodName, null);
-			m.invoke(this);
-			this.tearDown();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-}
+ 
+ public class TestCase {
+ 
+ 	String methodName;
+ 
+ 	public TestCase(String methodName) {
+ 		this.methodName = methodName;
+ 	}
+ 
+ 	public void setUp() {
+ 
+ 	}
+ 	
+ 	public void tearDown() {
+ 
+ 	}
+ 
+	public TestResult run() {
+		TestResult result = new TestResult();
+ 		Class cls = this.getClass();
+ 		try {
+ 			this.setUp();
+			result.testStarted();
+ 			Method m = cls.getDeclaredMethod(methodName, null);
+ 			m.invoke(this);
+ 			this.tearDown();
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
+		return result;
+ 	}
+ 
+ }
