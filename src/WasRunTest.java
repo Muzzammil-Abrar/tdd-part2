cb427d3a10952c659676fb54056878b9b7f1bbec
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 16:32:15 +0500

    Chapter No 18 : Invoke test method.
    Chapter No 18 : Invoke test method.

---
+++ src/WasRunTest.java
@@ -0,0 +1,17 @@
+package tddPart2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WasRunTest {
+
+	@Test
+	public void test() {
+		WasRun test = new WasRun("testMethod");
+		assertFalse(test.wasRun);
+		test.run();
+		assertTrue(test.wasRun);
+	}
+
+}
