9a167d40136256a70bb7653c7ae2595dc6a949d2
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:26:29 +0500

    Chapter 23: Can run multiple test.
    Chapter 23: Can run multiple test.

--- src/TestCaseTest.java
+++ src/TestCaseTest.java
@@ -2,41 +2,60 @@ package tddPart2;
 
 public class TestCaseTest extends TestCase {
  	WasRun test = null;
+	TestResult result;
  
  	public TestCaseTest(String methodName) {
  		super(methodName);
  	}
+	
+	public void setUp() {
+		result = new TestResult();
+	}
  
  	public void testTemplateMethod() {
  		WasRun test = new WasRun("testMethod");
- 		test.run();
+		test.run( result );
  		System.out.println(test.log.equals("setUp testMethod tearDown "));
  	}
- 	
- 	public void testResult(){
+
+	public void testResult() {
  		WasRun test = new WasRun("testMethod");
- 		TestResult result = test.run();
+-		TestResult result = test.run();
+		test.run( result );
  		System.out.println("1 run, 0 failed".equals(result.summary()));
  	}
- 	
+
  	public void testFailedResult() {
  		WasRun test = new WasRun("testBrokenMethod");
- 		TestResult result = test.run();
+		test.run( result );
  		System.out.println("1 run, 1 failed".equals(result.summary()));
  	}
-	
-	public void testFailedResultFormatting() {
-		TestResult result = new TestResult();
-		result.testStarted();
-		result.testFailed();
-    	System.out.println("1 run, 1 failed".equals(result.summary()));
-	}
+
+ 	public void testFailedResultFormatting() {
+ 		TestResult result = new TestResult();
+ 		result.testStarted();
+ 		result.testFailed();
+ 		System.out.println("1 run, 1 failed".equals(result.summary()));
+ 	}
  
+	public void testSuite() {
+		TestSuite suite = new TestSuite();
+		suite.add(new WasRun("testMethod"));
+		suite.add(new WasRun("testBrokenMethod"));
+		suite.run( result );
+		System.out.println("2 run, 1 failed".equals(result.summary()));
+	}
+
  	public static void main(String[] args) {
- 		new TestCaseTest("testTemplateMethod").run();
- 		new TestCaseTest("testResult").run();
- 		new TestCaseTest("testFailedResult").run();
-		new TestCaseTest("testFailedResultFormatting").run();
+		TestSuite suite = new TestSuite();
+		suite.add(new TestCaseTest("testTemplateMethod"));
+		suite.add(new TestCaseTest("testResult"));
+		suite.add(new TestCaseTest("testFailedResult"));
+		suite.add(new TestCaseTest("testFailedResultFormatting"));
+		suite.add(new TestCaseTest("testSuite"));
+		TestResult result = new TestResult();
+		suite.run(result);
+		System.out.println(result.summary());
  	}
  
  }
