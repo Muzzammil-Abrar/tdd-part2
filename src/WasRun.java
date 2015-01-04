c3cbb89afbb0f5ed0d3598d40ef9d3d78c358f6a
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 16:33:24 +0500

    Chapter No 18 : Invoke test method.
    Chapter No 18 : Invoke test method.

---
+++ src/WasRun.java
@@ -0,0 +1,16 @@
+package tddPart2;
+
+public class WasRun extends TestCase {
+	boolean wasRun;
+
+	public WasRun(String methodName) {
+		super(methodName);
+		wasRun = false;
+
+	}
+
+	public void testMethod() {
+		wasRun = true;
+	}
+
+}
