07da275981414f6f9997277aba3a373cd3f21174
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:08:13 +0500

    Chapter 21: Report Collected Results
    Chapter 21: Report Collected Results

--- src/TestCaseTest.java
+++ src/TestCaseTest.java
@@ -6,28 +6,29 @@ public class TestCaseTest extends TestCase {
  	public TestCaseTest(String methodName) {
  		super(methodName);
  	}
-	
-	public void setUp() {
-		test = new WasRun("testMethod");
-	}
  
-	public void testRunning() {
-		test.run();
-		System.out.println(test.wasRun);
+ 	public void testTemplateMethod() {
+ 		WasRun test = new WasRun("testMethod");
+ 		test.run();
+ 		System.out.println(test.log.equals("setUp testMethod tearDown "));
+ 	}
+	
+	public void testResult(){
+		WasRun test = new WasRun("testMethod");
+		TestResult result = test.run();
+		System.out.println("1 run, 0 failed".equals(result.summary()));
 	}
 	
-	public void testSetUp() {}
-	public void testTemplateMethod() {
-		WasRun test = new WasRun("testMethod");
- 		test.run();
-		System.out.println(test.wasSetUp);
-		System.out.println(test.log.equals("setUp testMethod tearDown "));
- 	}
+	public void testFailedResult() {
+		WasRun test = new WasRun("testBrokenMethod");
+		TestResult result = test.run();
+		System.out.println("1 run, 1 failed".equals(result.summary()));
+	}
  
  	public static void main(String[] args) {
-		new TestCaseTest("testRunning").run();
-		new TestCaseTest("testSetUp").run();
-		new TestCaseTest("testTemplateMethod").run();
+ 		new TestCaseTest("testTemplateMethod").run();
+		new TestCaseTest("testResult").run();
+		new TestCaseTest("testFailedResult").run();
  	}
  
  }
