68e0686a26c80904cfd56ce46315cf52f5c7dc0c
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 17:15:25 +0500

    Chapter 22: Added Log for  failed results
    Chapter 22: Added Log for  failed results

--- src/TestResult.java
+++ src/TestResult.java
@@ -1,14 +1,19 @@
 package tddPart2;
 
 public class TestResult {
-	int runCount = 0;
-	
-	public void testStarted(){
-		runCount++;
+ 	int runCount = 0;
+	int errorCount = 0;
+ 	
+ 	public void testStarted(){
+ 		runCount++;
+ 	}
+ 
+ 	public String summary() {
+		return runCount + " run, " + errorCount + " failed";
 	}
 
-	public String summary() {
-		return runCount + " run, 0 failed";
-	}
-
-}
+	public void testFailed() {
+		errorCount++;
+ 	}
+ 
+ }
