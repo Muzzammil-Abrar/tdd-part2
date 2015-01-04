20232126d702376d622660ecd86b5fdc05e8714e
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-04 16:45:56 +0500

    Chapter No 19 : Invoke setUp() First
    Chapter No 19 : Invoke setUp() First

--- src/WasRun.java
+++ src/WasRun.java
@@ -1,16 +1,22 @@
 package tddPart2;
 
 public class WasRun extends TestCase {
-	boolean wasRun;
-
-	public WasRun(String methodName) {
-		super(methodName);
+ 	boolean wasRun;
+	boolean wasSetUp;
+ 
+ 	public WasRun(String methodName) {
+ 		super(methodName);
 		wasRun = false;
 
+ 	}
+ 
+ 	public void testMethod() {
+ 		wasRun = true;
+ 	}
+	
+	public void setUp() {
+		wasRun = false;
+		wasSetUp = true;
 	}
-
-	public void testMethod() {
-		wasRun = true;
-	}
-
-}
+ 
+ }
